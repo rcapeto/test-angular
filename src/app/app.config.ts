55ea042e6534296e2991f86977c394efa28f1584
rcapeto <raphaelcapeto@gmail.com>
2024-04-11 22:21:10 -0300

    feat: add http provider and material ui icons in application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync('noop'),
+    importProvidersFrom(HttpClientModule),
+  ]
 };
