a4b5b06ae60f50cbe6b2942d9e87d0d50ae56709
rcapeto <raphaelcapeto@gmail.com>
2024-04-11 22:20:15 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '~/app/pages/home/home.component'
+import { UserComponent } from '~/app/pages/user/user.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: HomeComponent },
+   { path: 'users/:username', component: UserComponent },
+];
