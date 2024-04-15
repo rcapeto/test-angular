4e3250c4c58b3169be90288f613f2eebfb19860a
rcapeto <raphaelcapeto@gmail.com>
2024-04-14 22:41:03 -0300

    feat: create getRelativeDate

---
+++ src/app/utils/getRelativeDate.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
+
+export function formatRelativeDate(date: Date | string): string {
+  return dayjs().to(date)
+}
